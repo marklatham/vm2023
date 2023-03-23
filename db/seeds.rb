41629d0333d5168d804b862e6be589ca29e59811
ankur <rordev@ongraph.com>
2023-03-23 18:40:59 +0530

    Add default admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "--------------------------------------------------------\n"
+puts "Default admin User created"
+admin_user = AdminUser.create!({
+	email: 'admin@example.com',
+  password: 'password',
+  password_confirmation: 'password'
+})
+puts "--------------------------------------------------------\n"
