4fae40626bef2bfb88047dfc4b35d135ae7fef88
ankur <rordev@ongraph.com>
2023-03-29 13:30:42 +0530

    Remove Default admin from seed.rb file and remove sign_up routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,3 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
-puts "--------------------------------------------------------\n"
-puts "Default admin User created"
-admin_user = AdminUser.create!({
-	email: 'admin@example.com',
-  password: 'password',
-  password_confirmation: 'password'
-})
-puts "--------------------------------------------------------\n"
